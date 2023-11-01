06d3bbcc1d2e61f44ee5ced9dee7c6ad83c656ba
Sara Brumfield <sarabrumfield@gmail.com>
2023-11-01 10:07:09 -0400

    job profile drop down based on union
    formstack limits dropdown to 1000 items

--- prop_new_PINs.R
+++ prop_new_PINs.R
@@ -84,3 +84,60 @@ for (a in agencies) {
        ),
        encode = "json")
 }
+
+
+classes <- import("C:/Users/sara.brumfield2/OneDrive - City Of Baltimore/_Code/_ref/Job_Classifications.xlsx")
+
+classes <- classes %>%
+  group_by(`Job Profile`, Union = `Job Families and Groups on Job Profile`) %>%
+  summarise(n()) %>%
+  ungroup() %>%
+  select(-`n()`) %>%
+  mutate(Union = case_when(is.na(Union) ~ "Unknown",
+                           TRUE ~ Union)) %>%
+  distinct()
+
+unions <- unique(classes$Union)
+
+jobs <- unique(classes$`Job Profile`)
+
+export_excel(data.frame(unions), "Unions", "Unions.xlsx")
+
+##push drop-down values to Formstack via API ============
+api <- bbmR::connect_fs_api()
+form_id = 5508140
+field_id = 154017457
+key = "7cf3e390462e4e0882dcc5df52a73d69"
+url <- paste0("https://www.formstack.com/api/v2/form/", form_id, "/field.json")
+
+api2 <- GET(paste0("https://www.formstack.com/api/v2/form.json?oauth_token=", key))
+http_status(api2)
+headers(api2)
+# content(api2, "text")
+
+
+for (u in unions) {
+  df <- classes %>% 
+    filter(`Union` == u) 
+  
+  POST(url,
+       add_headers(Authorization = paste("Bearer", #Sys.getenv("FS_API_KEY"), sep = " ")),
+                                         key, sep = " ")),
+       content_type("application/json"),
+       accept("application/json"),
+       body = list(id = form_id,
+                   field_type = "select",
+                   label = paste(u, ": Job Profile"),
+                   options = as.list(c(NA, sort(unique(df$`Job Profile`)))),
+                   required = 0,
+                   logic = list(
+                     action = "show",
+                     conditional = "all",
+                     checks = list(list(
+                       condition = "equals",
+                       field = field_id,
+                       option = u))
+                   )
+       ),
+       encode = "json")
+}
